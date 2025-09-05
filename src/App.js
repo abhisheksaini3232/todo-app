11a298ce19988e7cf040b202b7b260643eafe880
abhisheksaini3232 <abhishekbadthunia3232@gmail.com>
2025-09-06 04:39:35 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import { getPosts } from './api';
 import { useEffect, useState } from "react";
 
 function App() {
   useEffect(() => {
-    getPosts().then(posts => console.log(posts);
-  }), [])
+    getPosts().then(posts => console.log(posts));
+  }, [])
   return (
     <>
 
