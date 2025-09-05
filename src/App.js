a1d5b931f119e547e1e05d5a7fcf348492bb3c14
abhisheksaini3232 <abhishekbadthunia3232@gmail.com>
2025-09-06 05:07:48 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-// import logo from "./logo.svg";
 import "./App.css";
-import { getPosts } from './api';
+import { getPosts } from "./api";
 import { useEffect, useState } from "react";
 
+function TitleSearch({ data }) {
+  const [query, setQuery] = useState("");
+
+  // Filter titles where query is a substring (case-insensitive)
+  const filteredTitles = data
+    .map((item) => item.title)
+    .filter((title) => title.toLowerCase().includes(query.toLowerCase()));
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search titles"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+
+      <ul>
+        {filteredTitles.map((title, index) => (
+          <li key={index}>{title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
+
   useEffect(() => {
-    getPosts().then(posts => setData(posts));
-  }, [])
+    getPosts().then((posts) => setData(posts));
+  }, []);
+
   return (
     <>
+      {data.length > 0 ? (
+        <ul>
+          {data.map((e) => (
+            <li key={e.id}>{e.title}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No Data</p>
+      )}
 
-       {data ? data.map((e) => <li>{e.title}</li>) : <p>No Data</p>}
-  
+      <TitleSearch data={data} />
     </>
-  )
+  );
 }
+
 export default App;
